e54faf9600a28de7a88b089e9b3727b2cc447ebd
Yura Sabat <yrasabat@gmail.com>
2022-10-11 23:37:44 +0300

    17

--- src/components/AddComment/AddComment.js
+++ src/components/AddComment/AddComment.js
@@ -1,9 +1,33 @@
 import React, {useState} from 'react';
 import styles from "./AddComment.module.css";
+import {useDispatch, useSelector} from "react-redux";
+import {addComment} from "../../redux/action-creators/Comments";
 
-const AddComment = ({setIsAdding}) => {
+const AddComment = ({setIsAdding, productId}) => {
 
     const [comment, setComment] = useState('');
+
+    const dispatch = useDispatch();
+
+
+    const {comments} = useSelector(({comments: {comments}}) => ({comments}));
+
+
+    console.log(comments);
+    const onAddComment = () => {
+
+        if(comment) {
+            const item = {
+                id: parseInt(comments.length+1),
+                productId:  parseInt(productId),
+                description: comment,
+                date:  Date.now().toLocaleString()
+            }
+
+
+            dispatch(addComment(item));
+        }
+    }
 
     return (
         <div className={styles.addForm}>
@@ -19,7 +43,7 @@ const AddComment = ({setIsAdding}) => {
                         />
                 </div>
                 <div className={styles.buttons}>
-                    <button className={styles.button}>Send</button>
+                    <button className={styles.button} onClick={onAddComment}>Send</button>
                     <button className={styles.button} onClick={() => setIsAdding(false)}>Cansel</button>
                 </div>
             </div>
--- src/components/ChosenItem/ChosenItem.js
+++ src/components/ChosenItem/ChosenItem.js
@@ -16,12 +16,15 @@ const ChosenItem = () => {
         product,
         chosenComments
     }));
+
+    const {comments} = useSelector(({comments: {comments}}) => ({comments}));
+
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getProductById(id));
         dispatch(getCommentByProductId(id));
-    }, []);
+    }, [comments]);
 
     // console.log(product);
 
@@ -30,46 +33,54 @@ const ChosenItem = () => {
 
     console.log(isAdding);
     return (
-        <div className={styles.main}>
-            <div className={styles.photo}>
-                <img src={`${product?.imageUrl}`} alt={product.name} className={styles.image}/>
+        <div>
+
+            <div className={styles.main}>
+                <div className={styles.photo}>
+                    <img src={`${product?.imageUrl}`} alt={product.name} className={styles.image}/>
+                </div>
+                <div className={styles.description}>
+                    <div style={{fontSize: 30, paddingBottom: 20}}>{product.name}</div>
+                    <div style={{fontSize: 20}}>Ціна: <b>{product.count} грн</b></div>
+                    <div style={{fontSize: 20}}>Об'єм: <b>{product.weight}</b></div>
+                </div>
+
             </div>
-            <div className={styles.description}>
-                <div style={{fontSize: 30, paddingBottom: 20}}>{product.name}</div>
-                <div style={{fontSize: 20}}>Ціна: <b>{product.count} грн</b></div>
-                <div style={{fontSize: 20}}>Об'єм: <b>{product.weight}</b></div>
-            </div>
+
             <div>
                 <Link to={`edit`} className={styles.editLink}>
                     Edit
                 </Link>
 
+            </div>
+            <div className={styles.commentsDiv}>
+                <div className={styles.commentsTitle}>Comments:</div>
+                {
+                    chosenComments.map(el => <div key={el.id}>
+                        <div className={styles.commentText}>{el.description}</div>
+                        <div className={styles.commentDate}>Published: {el.date}
+                            <div>Edit</div>
+                            <div>Delete</div>
+                        </div>
+                    </div>)
+                }
 
-                <div className={styles.commentsDiv}>
-                    <div className={styles.commentsTitle}>Comments:</div>
+                <button
+                    onClick={() => setIsAdding(true)}
+                    className={styles.addComment}
+                >
+                    Add comment
+                </button>
+                <div>
                     {
-                        chosenComments.map(el => <div key={el.id}>
-                            <div className={styles.commentText}>{el.description}</div>
-                            <div className={styles.commentDate}>Published: {el.date} <div>Edit</div><div>Delete</div></div>
-                        </div>)
+                        isAdding ? <AddComment
+                            productId={id}
+                            setIsAdding={setIsAdding}
+                        /> : null
                     }
-
-                    <button
-                        onClick={() => setIsAdding(true)}
-                        className={styles.addComment}
-                    >
-                        Add comment
-                    </button>
-                    <div>
-                        {
-                            isAdding ? <AddComment setIsAdding={setIsAdding}/> : null
-                        }
-                    </div>
                 </div>
             </div>
         </div>
-
-
     );
 };
 
